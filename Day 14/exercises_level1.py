49ad988fbbf17f60ea7c7c3e6f76b981b6a17838
Ankush Karki <ankushdhojkarki@gmail.com>
2026-01-14 20:50:34 +0545

    Day 13 exercise 1

---
+++ Day 14/exercises_level1.py
@@ -0,0 +1,18 @@
+#1. Use for loop to print each country in the countries list.
+countries = ["Estonia", "Finland", "Sweden", "Denmark"]
+
+for country in countries:
+    print(country)
+
+
+#2. Use for to print each name in the names list.
+names = ["Asabeneh", "Lidiya", "Ermias", "Abraham"]
+
+for name in names:
+    print(name)
+
+#3. Use for loop to print each number in the numbers list.
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
